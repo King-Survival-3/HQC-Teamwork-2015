15519b5e43bfed208476566314714728e0cf82a8
georgimanov <georgimanov@gmail.com>
2015-09-29 15:06:42 +0300

    [Add] Console Renderer

---
+++ KingSurvival Game/KingSurvival.Chess/Renderer/ConsoleRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KingSurvival.Chess.Renderer
+{
+    using System.Threading;
+
+    using KingSurvival.Chess.Board.Contracts;
+    using KingSurvival.Chess.Common.Console;
+    using KingSurvival.Chess.Renderer.Contrats;
+
+    public class ConsoleRenderer : IRenderer
+    {
+        private const string Logo = "Just Chess";
+
+        public void RenderMainMenu()
+        {
+            ConsoleHelpers.SetCursorAtCenter(Logo.Length);
+            Console.WriteLine(Logo);
+
+            // TODO: Add main menu
+            Thread.Sleep(1000);
+        }
+
+        public void RenderBoard(IBoard board)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
