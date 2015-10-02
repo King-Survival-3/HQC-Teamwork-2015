b55f55cf33da8f4bb7aa1a5eee732368b208668d
Geogri Manov <georgimanov@gmail.com>
2015-10-02 12:43:43 +0300

    [Add] Normal Pawn Movement

--- KingSurvival Game/KingSurvival.Chess/Movements/NormalPawnMovement.cs
+++ KingSurvival Game/KingSurvival.Chess/Movements/NormalPawnMovement.cs
@@ -1,6 +1,107 @@
 ﻿namespace KingSurvival.Chess.Movements
 {
-    public class NormalPawnMovement
+    using System;
+
+    using KingSurvival.Chess.Movements.Contracts;
+    using KingSurvival.Chess.Board.Contracts;
+    using KingSurvival.Chess.Common;
+    using KingSurvival.Chess.Figures.Contracts;
+
+    public class NormalPawnMovement : IMovement
     {
+        private const string PawnBackwardsErrorMessage = "Pawns cannot move backwards!";
+        private const string PawnInvalidMove = "Pawns cannot move this way!";
+
+        public void ValidateMove(IFigure figure, IBoard board, Move move)
+        {
+            var color = figure.Color;
+            var other = figure.Color == ChessColor.White ? ChessColor.White : ChessColor.Black;
+            var from = move.From;
+            var to = move.To;
+
+
+            if (color == ChessColor.White &&
+                to.Row < from.Row)
+            {
+                throw new InvalidOperationException(PawnBackwardsErrorMessage);
+            }
+
+            if (color == ChessColor.Black && to.Row > from.Row)
+            {
+                throw new InvalidOperationException(PawnBackwardsErrorMessage);
+            }
+
+            if (color == ChessColor.White)
+            {
+                if (from.Row + 1 == to.Row && this.CheckDiagonalMove(from, to))
+                {
+                    if (this.CheckOtherFigureIfValid(board, to, other))
+                    {
+                        return;
+                    }
+                }
+            }
+            else if (color == ChessColor.Black)
+            {
+                if (from.Row - 1 == to.Row && this.CheckDiagonalMove(from, to))
+                {
+                    if (this.CheckOtherFigureIfValid(board, to, other))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            // TODO: remove 2 magic number (const)
+            if (from.Row == 2 && color == ChessColor.White)
+            {
+                if (from.Row + 2 == to.Row &&
+                    this.CheckOtherFigureIfValid(board, to, other))
+                {
+                    return;
+                }
+            }
+            else if (from.Row == 7 && color == ChessColor.Black)
+            {
+                if (from.Row - 2 == to.Row &&
+                    this.CheckOtherFigureIfValid(board, to, other))
+                {
+                    return;
+                }
+            }
+
+            if (from.Row + 1 == to.Row && color == ChessColor.White)
+            {
+                if (this.CheckOtherFigureIfValid(board, to, other))
+                {
+                    return;
+                }
+            }
+            else if (from.Row - 1 == to.Row && color == ChessColor.Black)
+            {
+                if (this.CheckOtherFigureIfValid(board, to, other))
+                {
+                    return;
+                }
+            }
+
+            throw new InvalidOperationException(PawnInvalidMove);
+        }
+
+        private bool CheckOtherFigureIfValid(IBoard board, Position to, ChessColor color)
+        {
+            var otherFigure = board.GetFigureAtPosition(to);
+            if (otherFigure != null && otherFigure.Color == color)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool CheckDiagonalMove(Position from, Position to)
+        {
+            return (from.Col + 1 == to.Col || from.Col - 1 == to.Col);
+        }
     }
 }
