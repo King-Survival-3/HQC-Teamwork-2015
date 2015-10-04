976bdf3a89cf1d8c49a47018c1bb6029808e3247
unknown <on.tomas@gmail.com>
2015-10-04 16:35:35 +0300

    added WebInputProvider

---
+++ KingSurvival Game/KingSurvival.Chess/InputProvider/WebInputProvider.cs
@@ -0,0 +1,36 @@
+﻿namespace KingSurvival.Chess.InputProvider
+{
+    using System;
+    using System.Collections.Generic;
+
+    using KingSurvival.Chess.Common;
+    using KingSurvival.Chess.Common.Console;
+    using KingSurvival.Chess.InputProvider.Contracts;
+    using KingSurvival.Chess.Players;
+    using KingSurvival.Chess.Players.Contracts;
+
+    class WebInputProvider : IInputProvider
+    {
+        private Move move;
+
+        public WebInputProvider(Move move)
+        {
+            this.move = move;
+        }
+
+        public IList<IPlayer> GetPlayers(int numberOfPlayers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Move GetNextPlayerMove(IPlayer player)
+        {
+            return this.move;
+        }
+
+        public GameType GetGameType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
