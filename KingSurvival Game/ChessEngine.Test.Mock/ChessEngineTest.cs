bea0ea28b5933acafd50ed0356dda35cf76110cb
oggg <ogggster@gmail.com>
2015-10-11 00:58:59 +0300

    moq test added

---
+++ KingSurvival Game/ChessEngine.Test.Mock/ChessEngineTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Renderer.Test.Mock
+{
+    using KingSurvival.Chess.Common;
+    using KingSurvival.Chess.Figures.Contracts;
+    using KingSurvival.Chess.Players;
+    using KingSurvival.Chess.Players.Contracts;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class MoqPlayer
+    {
+        [TestMethod]
+        public void CheckPlayerProperties()
+        {
+            var firstPlayer = new Player("Pesho", ChessColor.White);            
+            var moqedplayers = new Mock<IPlayer>();
+            moqedplayers.Setup(p => p.AddFigure(It.IsAny<IFigure>())).Verifiable();
+            moqedplayers.Setup(p => p.Name).Returns(firstPlayer.Name);
+            moqedplayers.Setup(p => p.Color).Returns(firstPlayer.Color);
+        }
+    }
+}
